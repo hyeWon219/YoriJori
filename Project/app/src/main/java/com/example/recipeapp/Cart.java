d363e962bb0eda43fb2302f5d424a845bb82703a
CHW <72531318+hyeWon219@users.noreply.github.com>
2022-09-12 00:41:22 +0900

    onClick 임시 추가

--- Project/app/src/main/java/com/example/recipeapp/Cart.java
+++ Project/app/src/main/java/com/example/recipeapp/Cart.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 public class Cart extends AppCompatActivity {
@@ -26,6 +27,23 @@ public class Cart extends AppCompatActivity {
                 Intent backIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(backIntent);
                 finish();
+            }
+        });
+
+        FrameLayout recipeFrame = findViewById(R.id.recipeFrame);
+        Button recipeDown = findViewById(R.id.button11);
+        recipeDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        FrameLayout reicpe1 = findViewById(R.id.recipe_1);
+        reicpe1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
             }
         });
     }
