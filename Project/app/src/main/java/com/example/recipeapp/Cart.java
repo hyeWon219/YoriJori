42e7617fcf30b581706732c378cfdaad83efba9f
CHW <72531318+hyeWon219@users.noreply.github.com>
2022-09-12 21:25:19 +0900

    레시피 창 ui 추가 및 온클릭 부여

--- Project/app/src/main/java/com/example/recipeapp/Cart.java
+++ Project/app/src/main/java/com/example/recipeapp/Cart.java
@@ -39,8 +39,129 @@ public class Cart extends AppCompatActivity {
             }
         });
 
+        //리스트 온클릭(노동...)
         FrameLayout reicpe1 = findViewById(R.id.recipe_1);
         reicpe1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe2 = findViewById(R.id.recipe_2);
+        reicpe2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe3 = findViewById(R.id.recipe_3);
+        reicpe3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe4 = findViewById(R.id.recipe_4);
+        reicpe4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe5 = findViewById(R.id.recipe_5);
+        reicpe5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe6 = findViewById(R.id.recipe_6);
+        reicpe6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe7 = findViewById(R.id.recipe_7);
+        reicpe7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe8 = findViewById(R.id.recipe_8);
+        reicpe8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe9 = findViewById(R.id.recipe_9);
+        reicpe9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe10 = findViewById(R.id.recipe_10);
+        reicpe10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe11 = findViewById(R.id.recipe_11);
+        reicpe11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe12 = findViewById(R.id.recipe_12);
+        reicpe12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe13 = findViewById(R.id.recipe_13);
+        reicpe13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe14 = findViewById(R.id.recipe_14);
+        reicpe14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe15 = findViewById(R.id.recipe_15);
+        reicpe15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipeFrame.setVisibility(View.VISIBLE);
+            }
+        });
+
+        FrameLayout reicpe16 = findViewById(R.id.recipe_16);
+        reicpe16.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 recipeFrame.setVisibility(View.VISIBLE);
