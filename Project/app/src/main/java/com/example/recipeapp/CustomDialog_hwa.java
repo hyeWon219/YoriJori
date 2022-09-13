59842bad2bd6912e511a9467120fc11ce3b11afc
hwafrome <kimkh6212@gmail.com>
2022-09-13 20:07:35 +0900

    recipe.java layout_recipe.xml 추가 layout_background.xml 수정
    병합하고 올릴때 CustomDialog_hwa, activty_recipe은 삭제부탁드립니다.

--- Project/app/src/main/java/com/example/recipeapp/CustomDialog_hwa.java
+++ Project/app/src/main/java/com/example/recipeapp/CustomDialog_hwa.java
@@ -1,4 +1,14 @@
 package com.example.recipeapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 public class CustomDialog_hwa {
+
+
+
 }
